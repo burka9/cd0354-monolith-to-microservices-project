a7bac9ecb0337008b02b2fef456e8b330abc44f2
Biruk Ephrem <birukeph@gmail.com>
2022-11-11 14:34:39 +0300

    separated services

---
+++ set_env.ps1
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+$env:POSTGRES_USERNAME="postgres"
+$env:POSTGRES_PASSWORD="postgres"
+$env:POSTGRES_HOST="postgres.cgdunwwfxnlh.us-east-1.rds.amazonaws.com"
+$env:POSTGRES_DB="postgres"
+$env:AWS_BUCKET="project3-101120995893"
+$env:AWS_REGION="us-east-1"
+$env:AWS_PROFILE="default"
+$env:JWT_SECRET="elloh"
+$env:URL="http://localhost:8100"
