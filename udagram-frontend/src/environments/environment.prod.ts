c3636d827dcab750324a573f4049a42a4d803efc
Biruk Ephrem <birukeph@gmail.com>
2022-11-15 16:35:44 +0300

    changed localhost to exposed url

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a71feb4ecc7e14f76aeec35e76335e46-1857960881.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
