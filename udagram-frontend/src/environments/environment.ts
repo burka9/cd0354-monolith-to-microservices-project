46a5547886ab26f2b2e1334bce8250b6e6a69e47
Biruk Ephrem <birukeph@gmail.com>
2022-11-21 21:18:02 +0300

    changed api host link

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a7bd5b8a615564414878e86fa8613c8f-773251248.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
